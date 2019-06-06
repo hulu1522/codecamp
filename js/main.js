72d90510cb008223d27a1c8d1848e3c1d63659ab
Jared Everett <jared.everett@tcnp3.com>
2019-06-06 16:22:45 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -99,12 +99,6 @@ function show(target){
 }
 
 window.onload = function() {
-	var startPos;
-	var geoSuccess = function(position) {
-	startPos = position;
-		document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-		document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-	};
 	navigator.geolocation.getCurrentPosition(geoSuccess);
 };
 
